6ec1c3ac42598da0d35c6aeea6bf27c0168f770e
TehWeifu <bvkvgxvt@gmail.com>
2021-09-23 12:57:53 +0200

    23/09/2021 BBDD practica4

--- PRG/Tema2/ExercicisIf/Exercici11.java
+++ PRG/Tema2/ExercicisIf/Exercici11.java
@@ -4,5 +4,15 @@ public class Exercici11 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int inputNum;
+		
+		System.out.print("Introduzca un numero entre 0 y 9.999: ");
+		inputNum = input.nextInt();
+		
+		if (inputNum > 9_999 || inputNum < 0) {
+			System.out.println("El numero introducido no es valido");
+		} else {
+			System.out.print("El numero " + inputNum + " tiene " + (int) Math.ceil(inputNum);			
+		}
 	}
 }
+
