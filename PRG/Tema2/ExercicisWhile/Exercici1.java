530cdba5a8fa967bf750299b084031d5647515c5
TehWeifu <bvkvgxvt@gmail.com>
2021-09-30 10:42:14 +0200

    30/09/2021 clase PRG Ejercicios while

---
+++ PRG/Tema2/ExercicisWhile/Exercici1.java
@@ -0,0 +1,16 @@
+// Exercici 1. Transforma el següent bucle for a un bucle while:
+// 	for (i=5; i<15; i++){
+//	 System.out.println(i);
+// 	}
+
+public class Exercici1 {
+	public static void main(String[] args) {
+		byte i = 5;
+		
+		while (i < 15) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
+
---
+++ PRG/Tema2/ExercicisWhile/Exercici2.java
@@ -0,0 +1,185 @@
+// Exercici 2: Realitza les següents figures: (Utilitza bucles while).
+
+public class Exercici2 {
+    public static void main(String[] args) {
+
+        // una línia composta per asteriscs (*) de 10 de larg. Cada impressió sols portarà un caràcter.
+        // ********** 
+        byte i1 = 0;
+        final byte SIZE1 = 10;
+
+        while (i1 < SIZE1) {
+            System.out.print('*');
+            i1++;
+        }
+        System.out.println();
+        System.out.println();
+
+
+        // una línia composta por uns (1) i ceros (0) intercalats de 20 de larg. Cada impressió sols portarà un caràcter.
+        // 10101010101010101010		
+        byte i2 = 1;
+        final byte SIZE2 = 20;
+
+        while (i2 <= SIZE2) {
+            System.out.print(i2 % 2);
+            i2++;
+        }
+        System.out.println();
+        System.out.println();
+
+
+        // una línia composta per seqüencies de quatre uns (1) i un cero (0) succesius de 25 de larg. 
+        // 1111011110111101111011110
+        byte i3 = 1;
+        final byte SIZE3 = 25;
+
+        while (i3 <= SIZE3) {
+            System.out.print(Math.min(1, (i3 % 5)));
+            i3++;
+        }
+        System.out.println();
+        System.out.println();
+
+
+        // un quadre compost per signes més (+) de 5 files i 10 columnes 
+        // ++++++++++ 
+        // ++++++++++
+        // ++++++++++
+        // ++++++++++
+        // ++++++++++          	
+        byte i4 = 0;
+        byte j4 = 0;
+        final byte ROWS4 = 5;
+        final byte COLUMNS4 = 10;
+
+        while (i4 < ROWS4) {
+            j4 = 0;
+
+            while (j4 < COLUMNS4) {
+                System.out.print('+');
+                j4++;
+            }
+
+            System.out.println();
+            i4++;
+        }
+        System.out.println();
+
+
+        // un quadre compost per signes igual (=) alternats amb signes menys (-) de 6 files i 10 columnes 
+        // =-=-=-=-=-
+        // =-=-=-=-=-
+        // =-=-=-=-=-
+        // =-=-=-=-=-
+        // =-=-=-=-=-
+        // =-=-=-=-=-  
+        byte i5 = 0;
+        byte j5 = 0;
+        final byte ROWS5 = 6;
+        final byte COLUMNS5 = 10;
+
+        while (i5 < ROWS5) {
+            j5 = 0;
+
+            while (j5 < COLUMNS5) {
+                if (j5 % 2 == 0)
+                    System.out.print('=');
+                else
+                    System.out.print('-');
+                j5++;
+            }
+
+            System.out.println();
+            i5++;
+        }
+        System.out.println();
+
+
+        // un triangle isósceles de 10 de altura, compost per lletres 'o' 
+        // o
+        // oo
+        // ooo
+        // oooo
+        // ooooo
+        // oooooo
+        // ooooooo
+        // oooooooo
+        // ooooooooo
+        // oooooooooo
+        byte i6 = 0;
+        byte j6 = 0;
+        final byte HEIGHT6 = 10;
+
+        while (i6 < HEIGHT6) {
+            j6 = 0;
+
+            while (j6 <= i6) {
+                System.out.print('o');
+                j6++;
+            }
+
+            System.out.println();
+            i6++;
+        }
+        System.out.println();
+
+
+        // el mateix triangle, però invertit i composto por lletres 'o' 
+        // oooooooooo
+        // ooooooooo
+        // oooooooo
+        // ooooooo
+        // oooooo
+        // ooooo
+        // oooo
+        // ooo
+        // oo
+        // o
+        byte i7 = 0;
+        byte j7 = 0;
+        final byte HEIGHT7 = 10;
+
+        while (i7 < HEIGHT7) {
+            j7 = (byte) (HEIGHT7 - i7);
+
+            while (j7 > 0) {
+                System.out.print('o');
+                j7--;
+            }
+
+            System.out.println();
+            i7++;
+        }
+        System.out.println();
+
+
+        // un rectangle buit compost per coixinets '#' de costats 10x5 
+        // ##########
+        // #        #
+        // #        #
+        // #        #
+        // ##########
+        byte i8 = 0;
+        byte j8 = 0;
+        final byte ROWS8 = 5;
+        final byte COLUMNS8 = 10;
+
+        while (i8 < ROWS8) {
+            j8 = 0;
+
+            while (j8 < COLUMNS8) {
+                if (i8 == 0 || (i8 == ROWS8 - 1) || j8 == 0 || (j8 == COLUMNS8 - 1))
+                    System.out.print('#');
+                else
+                    System.out.print(' ');
+                j8++;
+            }
+            System.out.println();
+            i8++;
+        }
+        System.out.println();
+    }
+}
+
+
