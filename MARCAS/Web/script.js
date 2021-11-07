3f457b11a44f76064f1d58b857bfed3889d7293c
TehWeifu <BVKVGXVT@gmai.com>
2021-11-07 22:23:47 +0100

    ED Guia SEO, Marcas Web actualizada, projectos Scratch

--- MARCAS/Web/script.js
+++ MARCAS/Web/script.js
@@ -3,16 +3,29 @@
 let cookiesDiv = document.querySelector('#cookies')
 let cookiesBtn = document.querySelector('#cookies button')
 
-setTimeout(() => {
-    cookiesDiv.style.width = "50%";
-    Array.from(cookiesDiv.children).forEach(child => {
-        setTimeout(() => {
-            child.style.display = "block"
-            setTimeout(() => child.style.opacity = "1", 500)
-        }, 2000)
-    })
-}, 1000)
+let cookiesAccepted = Boolean(window.localStorage.getItem("cookiesAccepted"))
+console.log(cookiesAccepted)
 
 cookiesBtn.addEventListener('click', () => {
     cookiesDiv.style.display = "none"
+    window.localStorage.setItem('cookiesAccepted', "true")
 })
+
+displayCookiesAgreement()
+
+function displayCookiesAgreement() {
+    if (!cookiesAccepted) {
+
+        setTimeout(() => {
+            cookiesDiv.style.width = "50%";
+            Array.from(cookiesDiv.children).forEach(child => {
+                setTimeout(() => {
+                    child.style.display = "block"
+                    setTimeout(() => child.style.opacity = "1", 500)
+                }, 2000)
+            })
+        }, 1000)
+    } else {
+        cookiesDiv.style.display = "none"
+    }
+}
