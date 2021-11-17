bb9b7ca043684d7907b122f0ca30828df918b368
TehWeifu <BVKVGXVT@gmai.com>
2021-11-17 22:39:52 +0100

    Marcas php y notas SEO

--- MARCAS/Web/EjerciciosMarcas/Formularios/php/phpForm1.php
+++ MARCAS/Web/EjerciciosMarcas/Formularios/php/phpForm1.php
@@ -1,35 +1,35 @@
-<?php 
+<?php
 
 //isset es verdadero si la variable que se le pasa como parámetro no está vacía
 
 //si se ha pulsado el botón de enviar formulario
-if (isset($_POST["sndBtn"])){
+if (isset($_POST["sndBtn"])) {
 
-        $userName = $_POST["userName"];
-        $userSur1 = $_POST["userSur1"];
-        $userSur2 = $_POST["userSur2"];
-        $userPass = $_POST["userPass"];
-        $userAdult = ($_POST["userAdult"] === "1" ? "Mayor de edad" : "Menor de edad");
-        $userSingle = ($_POST["userSingle"] === "1" ? "Soltero" : "Con pareja");
-        $userFavCountry = $_POST["userFavCountry"];
-        $userRegion = $_POST["userRegion"];
-        $userCountry = $_POST["userCountry"];
-        $userExtra = $_POST["userExtra"];
+    $userName = $_POST["userName"];
+    $userSur1 = $_POST["userSur1"];
+    $userSur2 = $_POST["userSur2"];
+    $userPass = $_POST["userPass"];
+    $userAdult = ($_POST["userAdult"] === "1" ? "Mayor de edad" : "Menor de edad");
+    $userSingle = ($_POST["userSingle"] === "1" ? "Soltero" : "Con pareja");
+    $userFavCountry = $_POST["userFavCountry"];
+    $userRegion = $_POST["userRegion"];
+    $userCountry = $_POST["userCountry"];
+    $userExtra = $_POST["userExtra"];
 
-        $mailMessage = $userName . " " . $userSur1 . " " . $userSur2 . "\n" .
-                        "Contraseña: " . $userPass . "\n" .
-                        $userAdult . "\n" .
-                        $userSingle . "\n" .
-                        "Pais favorito: " . $userFavCountry . "\n" .
-                        "Provincia: " . $userRegion . "\n" .
-                        "Pais: " . $userCountry . "\n" .
-                        "Comentarios: " . $userExtra . "\n";
+    $mailMessage = $userName . " " . $userSur1 . " " . $userSur2 . "\n" .
+        "Contraseña: " . $userPass . "\n" .
+        $userAdult . "\n" .
+        $userSingle . "\n" .
+        "Pais favorito: " . $userFavCountry . "\n" .
+        "Provincia: " . $userRegion . "\n" .
+        "Pais: " . $userCountry . "\n" .
+        "Comentarios: " . $userExtra . "\n";
 
-		 
-		//sintaxis mail(destino, asunto,mensaje,cabeceras);
-		mail("bvkvgxvt@gmail.com", "Formulario 1", $mailMessage, "From:uwu\n");
-		
-		//mensaje de confirmación de envío
-		echo "El formulario ha sido enviado con éxito.";
+
+    //sintaxis mail(destino, asunto,mensaje,cabeceras);
+    mail("bvkvgxvt@gmail.com", "Formulario 1", $mailMessage, "From:uwu\n");
+
+    //mensaje de confirmación de envío
+    echo "El formulario ha sido enviado con éxito.";
 }
 ?>
--- MARCAS/Web/EjerciciosMarcas/Formularios/php/phpForm3.php
+++ MARCAS/Web/EjerciciosMarcas/Formularios/php/phpForm3.php
@@ -1,30 +1,30 @@
-<?php 
+<?php
 
 //isset es verdadero si la variable que se le pasa como parámetro no está vacía
 
 //si se ha pulsado el botón de enviar formulario
-if (isset($_POST["sndBtn"])){
+if (isset($_POST["sndBtn"])) {
+
+    $userFirstName = $_POST["userFirstName"];
+    $userLastName = $_POST["userLastName"];
+    $userMail = $_POST["userMail"];
+    $userCountry = $_POST["userCountry"];
+    $userFrequency = $_POST["userFrequency"];
+    $userDescription = $_POST["userDescription"];
+    $userLanguages = $_POST["userLanguages"];
 
 
+    $mailMessage = $userFirstName . " " . $userLastName . " " . "\n" .
+        "Mail: " . $userMail . "\n" .
+        "Country: " . $userCountry . "\n" .
+        "Language: " . $userLanguages . "\n" .
+        "Description: " . $userDescription . "\n" .
+        "Translation frequency: " . $userFrequency . "\n";
 
-        $userFirstName = $_POST["userFirstName"];
-        $userLastName = $_POST["userLastName"];
-        $userMail = $_POST["userMail"];
-        $userCountry = $_POST["userCountry"];
-        $userFrequency = $_POST["userFrequency"];
-        $userDescription = $_POST["userDescription"];
-        $userLanguages = $_POST["userLanguages"];
+    //sintaxis mail(destino, asunto,mensaje,cabeceras);
+    mail("bvkvgxvt@gmail.com", "Formulario 3", $mailMessage, "From:owo\n");
 
-
-        $mailMessage = $userFirstName . " " . $userLastName . " " . "\n" .
-                        "Mail: " . $userMail . "\n" .
-                        "Country: " . $userCountry . "\n" .
-
-		 
-		//sintaxis mail(destino, asunto,mensaje,cabeceras);
-		mail("bvkvgxvt@gmail.com", "Formulario 2", $mailMessage, "From:owo\n");
-		
-		//mensaje de confirmación de envío
-		echo "El formulario ha sido enviado con éxito.";
+    //mensaje de confirmación de envío
+    echo "El formulario ha sido enviado con éxito.";
 }
 ?>
