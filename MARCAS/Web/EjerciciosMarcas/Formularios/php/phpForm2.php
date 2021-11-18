187a28276df92dbcf3429f1389c7e322be7cc147
TehWeifu <BVKVGXVT@gmai.com>
2021-11-18 22:24:54 +0100

    marcas apuntes y y php

---
+++ MARCAS/Web/EjerciciosMarcas/Formularios/php/phpForm2.php
@@ -0,0 +1,36 @@
+<?php
+
+//isset es verdadero si la variable que se le pasa como parámetro no está vacía
+
+//si se ha pulsado el botón de enviar formulario
+if (isset($_POST["sndBtn"])) {
+
+    $userName = $_POST["userName"];
+    $userSur = $_POST["userSur"];
+    $userMail = $_POST["userMail"];
+    $userCity = $_POST["userCity"];
+    $userRegion = $_POST["userRegion"];
+    $userAge = $_POST["userAge"];
+    $userHow = $_POST["userHow"];
+    $userOpinion = $_POST["userOpinion"];
+    $userSug = $_POST["userSug"];
+    $userInternetHours = $_POST["userInternetHours"];
+
+
+    $mailMessage = $userName . " " . $userSur . "\n" .
+        "Provincia: " . $userRegion . "\n" .
+        "Ciudad: " . $userCity . "\n" .
+        "Edad: " . $userAge . "\n" .
+        "Como nos conociste: " . $userHow . "\n";
+        "Opinion pagina: " . $userOpinion . "\n";
+        "Sugerencias: " . $userSug . "\n";
+        "Tiempo en Internet: " . $userInternetHours . "\n";
+
+
+    //sintaxis mail(destino, asunto,mensaje,cabeceras);
+    mail("bvkvgxvt@gmail.com", "Formulario 2", $mailMessage, "From:$userMail\n");
+
+    //mensaje de confirmación de envío
+    echo "El formulario ha sido enviado con éxito.";
+}
+?>
